57134f87af9ce215c21c028c7009864041158b9f
Sandeep <sandeep.g@melento.com>
2024-12-05 18:04:03 +0530

    [NEW] NetflixGPT Scaffhold. Description : 1)Create react app. 2)Initialised tailwind css to it.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Create React App
+
+- create new react app
+- Configured Tailwind css
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Welcome to NetflixGPT
+    </div>
+  );
+}
+
+export default App;
