82d9d8b0456273371443b347b888096ffd64fc45
Sandeep <sandeep.g@melento.com>
2024-12-26 18:11:40 +0530

    [NEW] Firebase setup Description : 1)Create project in firebase. 2)Setup firebase in local machine. 3)firebase login,firebase init,firebase deploy.

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # Create React App
-
 - create new react app
 - Configured Tailwind css
+- Header
+- Routing
+- Login & Signup
+- Form Validations
+- useRef Hook
+- Firebase setup for creating,hosting and deploying.
+
+## Features
+- Signup Component
+  - UI for login
+  - Form validation for signup
+- Login Component
+  - UI for Signup
+  - Form Validations for Login
+- Firebase Setup
+  - Create a project in firebase
+  - npm install -g firebase-tools - for getting firebase in our system
+  - firebase login
+  - firebase init
+  - firebase deploy
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6sukd4lrkQ9L7Rr8YjFdQgD4R7UyPyPs",
+  authDomain: "netflix-gpt-3024f.firebaseapp.com",
+  projectId: "netflix-gpt-3024f",
+  storageBucket: "netflix-gpt-3024f.firebasestorage.app",
+  messagingSenderId: "306189115477",
+  appId: "1:306189115477:web:fb0831ce4b5fdb8d3334dc",
+  measurementId: "G-81MN7KRHB3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
